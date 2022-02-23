c1cde4e6914302829ba949e5b4f4f1b532de8d6a
daniel-555-24 <54788655+daniel-555-24@users.noreply.github.com>
2022-02-23 19:24:48 +0700

    commit : test responsive

--- client/src/components/GettingStarted/Step3.jsx
+++ client/src/components/GettingStarted/Step3.jsx
@@ -1,33 +1,64 @@
-import React, { useState } from "react";
+import Logo from '../../Assets/Model List.png'
 import "./Modal.css";
 
-export default function Modal() {
-    const[modal,setModal] = useState(false);
-
-    const toggleModal = () => {
-        setModal(!modal)
-    }
+export default function StartedHomePage3() {
   return(
-    <div className="">
-        <div className="api-usage">
-         <p style={{color:"white",fontSize:"30px", left: "974px",top: "90px",fontWeight:"bold",position:"fixed"}}>Usage</p>
-         <p style={{color:"white",fontSize:"20px", left: "974px",top: "140px",fontWeight:"bold",position:"fixed"}}>Api Usage</p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "180px",position:"fixed"}}>This is the hit counter for the sentiment detection API.</p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "200px",position:"fixed"}}>Basically it counts how many times the API has been hit </p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "220px",position:"fixed"}}>to analyze sentences.</p>
-        </div> 
-        <div className="tfl-usage">
-         <p style={{color:"white",fontSize:"20px", left: "974px",top: "263px",fontWeight:"bold",position:"fixed"}}>Transfer Learning Usage</p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "303px",position:"fixed"}}>This is the dataset counter for the “Transfer Learning”</p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "323px",position:"fixed"}}>feature. It accumulates how many rows of sentences have </p>
-         <p style={{color:"white",fontSize:"16px", left: "974px",top: "343px",position:"fixed"}}>been uploaded by the user.</p>
-        </div> 
-    <div className="modal-content-wrapper2">
-      <div modal-content>
-      </div>
-    </div>
-  </div>
-  
-
+    <>
+      <section class="section">
+          <div class="container">
+            <div className="columns">
+              <div className="column is-8">
+              <figure>
+                <br/><br/><br/>
+                <img src={Logo} alt="tandai"/>
+              </figure>
+                </div>  
+              <div className="column is-4 has-text-left">
+                  <h1 className="is-size-3 title" style={{color:"white"}}>Model List</h1>
+                  <h2 className="is-size-4 subtitle" style={{color:"white",margin:"auto"}}>Model Id</h2>
+                  <p style={{color:"white",fontSize:"14px"}}>Model ID is a combination of unique special letters and numbers that are generated automatically. The model with the ID “lstmw13” is our baseline model.</p>
+                  <br />
+                  <h2 className="is-size-4 subtitle" style={{color:"white", margin:"auto"}}>Model Name</h2>
+                  <p style={{color:"white",fontSize:"14px"}}>Model Name is used as the display name of the model. It is editable, so that the user can easily recognize which model is which</p>
+                  <br />
+                  <h2 className="is-size-4 subtitle" style={{color:"white", margin:"auto"}}>Status</h2>
+                  <p style={{color:"white",fontSize:"14px"}}>Status provides you the conditions and progresses of the models.</p>
+                  <br />
+                  <div className="columns">
+                    <div className="column is-3">
+                        <span className="tag is-success is-normal">Ready</span>
+                    </div>
+                    <div className="column">
+                      <p style={{color:"white",fontSize:"14px"}}>The model is ready to be used.</p>
+                    </div>
+                  </div>
+                  <div className="columns">
+                    <div className="column is-3">
+                        <span className="tag is-warning is-normal">On Progress</span>
+                    </div>
+                    <div className="column">
+                      <p style={{color:"white",fontSize:"14px"}}>The model is currently being trained and is not available to use yet.</p>
+                    </div>
+                  </div>  
+                  <div className="columns">
+                    <div className="column is-3">
+                        <span className="tag is-danger is-normal">Error</span>
+                    </div>
+                    <div className="column">
+                      <p style={{color:"white",fontSize:"14px"}}>The training ends with an error and the model cannot be used.</p>
+                    </div>
+                  </div>
+                  <div className="columns">
+                    <div className="column is-2">
+                    </div>
+                    <div className="column">
+                      <p style={{color:"white",fontSize:"14px"}}>(PS: Please check the dataset uploaded for mismatch (refer to “CSV Convention” section) and/or contact us for further support.)</p>
+                    </div>
+                  </div>      
+              </div>
+            </div>
+          </div>
+      </section>
+    </>
   );
 }
